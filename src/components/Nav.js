90e86e6a04f0cc0ce97ccb805ed166c5f01d5ded
JD Martinez <focus4ursoul@gmail.com>
2020-12-30 15:51:52 -0800

    layout component structure and start header

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+
+export default function Nav() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Menu</li>
+                    <li>Order</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Header from "../components/Nav";
+
+export default function Home() {
+  return (
+    <section>
+      <h2>Test</h2>
+      <Header />
+    </section>
+  )
+}
